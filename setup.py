ea6f193409d339cae79c51f4f565638da587db38
piakoch <123562360+piakoch@users.noreply.github.com>
2025-08-25 18:11:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+import setuptools
 
+setuptools.setup(
+    name = "ultimate-Snake",
+    version = "0.0.1",
+    author = "piakoch",
+    author_email = "pia.koch@pmb-net.de",
+    python_requires = ">=3.6",
+    packages = setuptools.find_packages()
+)
